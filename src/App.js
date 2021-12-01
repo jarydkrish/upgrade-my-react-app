96404c58c97c7a0f556fde799762b7cbc5c2acf2
Jaryd Krishnan <jaryd@hey.com>
2021-11-30 18:21:29 -0600

    Use react 15

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Some content</p>
         <a
           className="App-link"
           href="https://reactjs.org"
